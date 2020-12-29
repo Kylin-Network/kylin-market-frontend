42aface48a27aac301443bd3b5f6e8eca160e53e
Billie Zhang <billiecoder@gmail.com>
2020-12-29 16:33:26 +0800

    add service detail page

--- packages/page-oracle-market/src/index.tsx
+++ packages/page-oracle-market/src/index.tsx
@@ -1,7 +1,12 @@
 import type { KeyedEvent } from '@polkadot/react-query/types';
 import * as React from 'react';
+import { useRef } from 'react';
+import { Route, Switch } from 'react-router';
+import Tabs from '@polkadot/react-components/Tabs';
 
 import { useTranslation } from './translate';
+import Main from './Main'
+import ServiceInfo from './ServiceInfo'
 
 interface Props {
   basePath: string;
@@ -11,8 +16,34 @@ interface Props {
 
 function OracleMarketApp({ basePath, className }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-
-  return <main className={className}>{t("page oracle market")}</main>;
+  const itemsRef = useRef([
+    {
+      isRoot: true,
+      name: 'service',
+      text: t<string>('Services')
+    },
+    {
+      hasParams: true,
+      name: 'query',
+      text: t<string>('Service details')
+    }
+  ]);
+  return (
+    <main className={className}>
+      <header>
+        <Tabs
+          basePath={basePath}
+          items={itemsRef.current}
+        />
+      </header>
+      <Switch>
+        <Route path={`${basePath}/query`}><ServiceInfo /></Route>
+        <Route>
+          <Main />
+        </Route>
+      </Switch>
+    </main>
+  );
 }
 
 export default React.memo(OracleMarketApp);
