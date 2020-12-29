7356f8a94480324846c8c7eb1f9d5c8884720e1c
Billie Zhang <billiecoder@gmail.com>
2020-12-29 22:43:38 +0800

    change the tab name

--- packages/page-oracle-market/src/index.tsx
+++ packages/page-oracle-market/src/index.tsx
@@ -19,8 +19,8 @@ function OracleMarketApp({ basePath, className }: Props): React.ReactElement<Pro
   const itemsRef = useRef([
     {
       isRoot: true,
-      name: 'services',
-      text: t<string>('Services')
+      name: 'overview',
+      text: t<string>('Marketplace')
     },
     {
       hasParams: true,
