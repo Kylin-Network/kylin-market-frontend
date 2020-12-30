a38738e9cc9e178d099645ede1fbf0cd6ce120f5
Billie Zhang <billiecoder@gmail.com>
2020-12-30 22:28:26 +0800

    remove data source details page

--- packages/page-oracle-data-sources/src/index.tsx
+++ packages/page-oracle-data-sources/src/index.tsx
@@ -1,12 +1,10 @@
 import type { KeyedEvent } from '@polkadot/react-query/types';
 import * as React from 'react';
 import { useRef } from 'react';
-import { Route, Switch } from 'react-router';
 import Tabs from '@polkadot/react-components/Tabs';
 
 import { useTranslation } from './translate';
 import Main from './Main'
-import DataSourceInfo from './DataSourceInfo'
 
 interface Props {
   basePath: string;
@@ -21,11 +19,6 @@ function OracleMarketApp({ basePath, className }: Props): React.ReactElement<Pro
       isRoot: true,
       name: 'overview',
       text: t<string>('Data sources')
-    },
-    {
-      hasParams: true,
-      name: 'query',
-      text: t<string>('Data source details')
     }
   ]);
   return (
@@ -36,12 +29,7 @@ function OracleMarketApp({ basePath, className }: Props): React.ReactElement<Pro
           items={itemsRef.current}
         />
       </header>
-      <Switch>
-        <Route path={`${basePath}/query`}><DataSourceInfo /></Route>
-        <Route>
-          <Main />
-        </Route>
-      </Switch>
+      <Main />
     </main>
   );
 }
