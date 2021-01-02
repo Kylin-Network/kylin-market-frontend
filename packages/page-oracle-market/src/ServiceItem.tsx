7c96993ac48a43b3f706d17bb8c776d65df6b645
Billie Zhang <billiecoder@gmail.com>
2021-01-02 20:58:10 +0800

    decorate the service list

--- packages/page-oracle-market/src/ServiceItem.tsx
+++ packages/page-oracle-market/src/ServiceItem.tsx
@@ -4,19 +4,59 @@ import type { Service } from "./types";
 
 interface Props {
   value: Service;
+  className?: string;
 }
 
-function ServiceItem({ value }: Props): React.ReactElement<Props> {
+function ServiceItem({ value, className = "" }: Props): React.ReactElement<Props> {
   return (
-    <div>
-      <img src={value.serviceThumb} />
-      <div>{value.serviceDataId}</div>
-      <div>{value.serviceName}</div>
-      <div>{value.serviceDesc}</div>
+    <div className={className}>
+      <div className="ui--ServiceItem-badge">
+        <img src={value.serviceThumb} />
+      </div>
+
+      <div className="ui--ServiceItem-info">
+        <h4 className="ui--ServiceItem-name">{value.serviceName}</h4>
+        <div className="ui--ServiceItem-desc">
+          <div>#{value.serviceDataId}</div>
+          <div>{value.serviceDesc}</div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default React.memo(styled(ServiceItem)`
-  width: 300px;
+  width: 25%;
+  display: flex;
+  margin-bottom: 24px;
+  .ui--ServiceItem-badge {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+    background-color: #ffffff;
+    & > img {
+      display: block;
+      max-width: 60% !important;
+      height: auto !important;
+      max-height: 55% !important;
+    }
+  }
+  .ui--ServiceItem-info {
+    padding: 0 16px;
+  }
+  .ui--ServiceItem-name {
+    color: #e6007a;
+    font-weight: 600;
+    font-size: 16px;
+  }
+  .ui--ServiceItem-desc {
+    color: #6a737d;
+    word-break: break-word;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    line-height: 1.25;
+  }
 `);
