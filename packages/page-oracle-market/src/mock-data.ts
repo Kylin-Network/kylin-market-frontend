f6bcf5170be477c49575035ecd38092c8677c458
Billie Zhang <billiecoder@gmail.com>
2021-01-05 12:48:05 +0800

    remove unused code

--- packages/page-oracle-market/src/mock-data.ts
+++ packages/page-oracle-market/src/mock-data.ts
@@ -126,16 +126,3 @@ function fromHex(h) {
   }
   return decodeURIComponent(escape(s));
 }
-
-let data: Service[] = [];
-
-for (let i = 10000; i < 10020; i++) {
-  data.push({
-    serviceName: `kylin-name-${i}`,
-    serviceDataId: i,
-    serviceDesc: `kylin-desc-${i}`,
-    serviceThumb: "https://avatars0.githubusercontent.com/ml/30?s=62&v=4",
-  });
-}
-
-export const serviceData = data;
